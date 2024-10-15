9b086c4adc10aff0382a0e66106916ce558bf559
Anil Nagvanshi <anil.nagvanshi@sap.com>
2024-10-15 17:23:17 +0000

    implemented custom action on entities with .on event and action/function

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -3,8 +3,11 @@ const cds = require("@sap/cds");
 class CatalogService extends cds.ApplicationService {
     init() {
         const { Books } = this.entities;
-
+        // Add discount for overstocked books
         this.after("READ", Books, this.grantDiscount);
+
+        //Reduce the stock of ordered books according to the ordered quantity
+        this.on("submitOrder", this.reduceStock);
 
         return super.init();
     }
@@ -15,6 +18,21 @@ class CatalogService extends cds.ApplicationService {
                 b.title += ' -- 11% Discount!';
             }
         }
+    }
+
+    reduceStock(req) {
+       /*  !!! This is only a preliminary, incomplete implementation of the submitOrder action !!!
+        !!! These will be used later to complete the implementaiton !!! */
+        const { Books } = this.entities;
+        const { book, quantity } = req.data;
+
+        if(quantity < 1) {
+            return req.error("The quantity must be atleast 1");
+        }
+
+        let stock = 10;
+
+        return { stock };
     }
 }
 
